bbde6f3c199912aa918a95c90e030a789274a3b7
Madhav Gupta <madhavg.ic.24@nitj.ac.in>
2025-07-13 21:56:42 +0530

    3rd commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings:["web developer","Eager Learner","Aspiring Software Developer","2nd Year Student"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
